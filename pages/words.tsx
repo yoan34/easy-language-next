a6ca971c49b9d26f5da940832be36f7e8cd7db45
yoan <yoan.bousquet@jokosun.com>
2023-05-25 08:52:00 +0200

    update

--- pages/words.tsx
+++ pages/words.tsx
@@ -1,22 +1,31 @@
 import csvParser from "csv-parser";
-import { useEffect } from "react";
-import nouns from '../../data/nouns.json';
+import { useEffect, useState } from "react";
+import verbs from '../../result.json';
 import { Converter } from "csvtojson/v2/Converter";
-
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 
 
 const csvFilePath = 'chemin/vers/le/fichier.csv';
 
+interface VerbProps {
+    english: string;
+    french: string;
+    level: string;
+    frequency: string;
+    type: string;
+    info: string;
+}
 
-const Word = ({noun}: any) => {
+const Word = (verb: VerbProps) => {
     return (
         <>
         <div className="word">
-            <div className="native cell-lg">{noun.english}</div>
-            <div className="to_learn cell-lg">{noun.french}</div>
-            <div className="level cell-sm">{noun.level}</div>
-            <div className="frequency cell-sm">{noun.frequency}</div>
-            <div className="info cell-md">{noun.type}</div>
+            <div className="native cell-lg">{verb.english}</div>
+            <div className="to_learn cell-lg">{verb.french}</div>
+            <div className="level cell-sm">{verb.level}</div>
+            <div className="frequency cell-sm">{verb.frequency}</div>
+            <div className="info cell-md">{verb.type}</div>
         </div>
         <style jsx>
             {`
@@ -39,13 +48,82 @@ const Word = ({noun}: any) => {
         </>
     )
 }
-
+function valuetext(value: number) {
+    return `${value}°C`;
+  }
 const AllWords = () => {
-    console.log(nouns)
+    const LEVEL = ["A1", "A2", "B1", "B2", "C1", "C2"];
+    const [value, setValue] = useState<number[]>([1, 10]);
+    const [allCheckbox, setAllCheckbox] = useState([true, true, true, true ,true, true])
+    
+
+    const handleChange = (event: Event, newValue: number | number[]) => {
+        setValue(newValue as number[]);
+    };
+
+    const handleAllCheckbox = (event) => {
+        console.log(parseInt(event.target.id) === 2)
+        const newAllCheckbox = [ ...allCheckbox ]
+        newAllCheckbox[parseInt(event.target.id) - 1] = !newAllCheckbox[parseInt(event.target.id) - 1]
+        setAllCheckbox(newAllCheckbox)
+    }
+
+    let filteredVerbs = verbs.filter((verb) => {
+        const index = LEVEL.indexOf(verb.level)
+        console.log(index)
+        return allCheckbox[index]
+    })
+    filteredVerbs = filteredVerbs.filter((verb) => {
+        return parseInt(verb.frequency) <= value[1] && parseInt(verb.frequency) >= value[0]
+    })
+
+
+    console.log(filteredVerbs)
+
     return (
         <>
         <div className="all-words">
-            {nouns.map(noun => <Word key={noun.english} noun={noun} />)}
+            <div>
+                <div className="container-input">
+                    <div>
+                        <label>A1</label>
+                        <input id="1" type="checkbox" checked={allCheckbox[0]} onChange={handleAllCheckbox} />
+                    </div>
+                    <div>
+                        <label>A2</label>
+                        <input id="2" type="checkbox" checked={allCheckbox[1]} onChange={handleAllCheckbox} />
+                    </div>
+                    <div>
+                        <label>B1</label>
+                        <input id="3" type="checkbox" checked={allCheckbox[2]} onChange={handleAllCheckbox} />
+                    </div>
+                    <div>
+                        <label>B2</label>
+                        <input id="4" type="checkbox" checked={allCheckbox[3]} onChange={handleAllCheckbox} />
+                    </div>
+                    <div>
+                        <label>C1</label>
+                        <input id="5" type="checkbox" checked={allCheckbox[4]} onChange={handleAllCheckbox} />
+                    </div>
+                    <div>
+                        <label>C2</label>
+                        <input id="6" type="checkbox" checked={allCheckbox[5]} onChange={handleAllCheckbox} />
+                    </div>
+                </div>
+                <Box sx={{ width: 100, pt: 2 }}>
+                    <Slider
+                        getAriaLabel={() => 'Temperature range'}
+                        value={value}
+                        onChange={handleChange}
+                        valueLabelDisplay="auto"
+                        getAriaValueText={valuetext}
+                        min={1}
+                        max={10}
+                    />
+                </Box>
+                <p>Total verbs: {filteredVerbs.length}</p>
+            </div>
+            {filteredVerbs.map((verb: VerbProps) => <Word key={verb.english} {...verb} />)}
         </div>
         <style jsx>
             {`
@@ -55,6 +133,9 @@ const AllWords = () => {
                 justify-content: center;
                 align-items: center;
                 background-color: #f9f9f9;
+            }
+            .container-input {
+                display: flex;
             }
             `}
         </style>
